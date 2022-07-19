9aba95d90074c15c8158c579531f55d7188bf986
Rohan Menezes <menezesr08@gmail.com>
2022-07-19 14:02:29 +0100

    Created landing screen

---
+++ lib/common/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const CustomButton({Key? key, required this.text, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: TextStyle(color: blackColor),
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: tabColor,
+        minimumSize: const Size(double.infinity, 50),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/features/landing/screens/landing_screen.dart';
 import 'package:whatsapp_clone/responsive/responsive.dart';
 import 'package:whatsapp_clone/screens/mobile_screen_layout.dart';
 import 'package:whatsapp_clone/screens/web_screen_layout.dart';
@@ -17,9 +18,6 @@ void main() async {
     title: 'Flutter Demo',
     debugShowCheckedModeBanner: false,
     theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
-    home: const ResponsiveLayout(
-      mobileScreenLayout: MobileScreenLayout(),
-      webScreenLayout: WebScreenLayout(),
-    ),
+    home: LandingScreen(),
   ));
 }
